6755a0746be77507032a8d573d1fcd856ddd4dc5
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-18 23:29:23 -0500

    add duplicate removal

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -4,5 +4,14 @@ from utils.helpers import ensure_dir
 
 
 def run_silver_job(spark):
+    print("Running Silver Cleaning Job...")
+
+    ensure_dir(SILVER_PATH)
+
+    df = spark.read.parquet(BRONZE_PATH)
+
+    cleaned = (
+        df.dropDuplicates(["order_id"])
+    )
 
     return cleaned
