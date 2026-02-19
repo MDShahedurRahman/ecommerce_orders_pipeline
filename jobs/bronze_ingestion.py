2210b5065c010e8b934512305c434a34dfeb21c2
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-18 23:23:56 -0500

    implement csv ingestion

--- jobs/bronze_ingestion.py
+++ jobs/bronze_ingestion.py
@@ -1,3 +1,18 @@
 from utils.schema import order_schema
 from utils.helpers import ensure_dir
 from config import BRONZE_PATH, SOURCE_FILE
+
+
+def run_bronze_job(spark):
+    print("Running Bronze Ingestion Job...")
+
+    ensure_dir(BRONZE_PATH)
+
+    df = (
+        spark.read.format("csv")
+        .option("header", True)
+        .schema(order_schema())
+        .load(SOURCE_FILE)
+    )
+
+    return df
