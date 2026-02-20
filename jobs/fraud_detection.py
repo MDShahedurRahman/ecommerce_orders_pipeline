cbab1898e7779b60d9064539371ea01a9cd677e7
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-19 23:43:08 -0500

    create fraud_flag column

--- jobs/fraud_detection.py
+++ jobs/fraud_detection.py
@@ -4,5 +4,12 @@ from utils.helpers import ensure_dir
 
 
 def run_fraud_detection(spark):
+    print("Running Fraud Detection Job...")
+
+    df = spark.read.parquet(SILVER_PATH)
+
+    flagged = df.withColumn(
+        "fraud_flag"
+    )
 
     return flagged
