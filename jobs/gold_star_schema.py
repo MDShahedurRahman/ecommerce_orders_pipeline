aab45dbbf56b3878a007363620af74829d161539
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-19 23:46:16 -0500

    build dim_customer

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -3,3 +3,12 @@ from utils.helpers import ensure_dir
 
 
 def run_gold_job(spark):
+    print("Running Gold Star Schema Job...")
+
+    ensure_dir(GOLD_PATH)
+
+    df = spark.read.parquet(SILVER_PATH)
+
+    dim_customer = df.select(
+        "customer_id", "customer_name", "country"
+    ).dropDuplicates()
