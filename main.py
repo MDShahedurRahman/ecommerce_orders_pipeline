036da385d07a9c8f95b159cd384d48791f831090
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-20 23:53:29 -0500

    Define spark session

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils.spark_session import get_spark_session
+
+from jobs.bronze_ingestion import run_bronze_job
+from jobs.silver_cleaning import run_silver_job
+from jobs.fraud_detection import run_fraud_detection
+from jobs.gold_star_schema import run_gold_job
+from jobs.business_kpis import run_kpi_job
+
+
+def main():
+    spark = get_spark_session()
+
+
+if __name__ == "__main__":
+    main()
